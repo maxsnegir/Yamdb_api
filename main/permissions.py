d5288887c4f9298a3e90aba040672ae03f502cbd
Максим <goshasnegir@icloud.com>
2021-04-08 11:42:54 +0500

    Some changes for PEP-8

--- main/permissions.py
+++ main/permissions.py
@@ -5,7 +5,7 @@ from rest_framework.permissions import BasePermission
 class IsAdminOrReadOnly(BasePermission):
     def has_permission(self, request, view):
         return request.method in permissions.SAFE_METHODS or (
-                request.user.is_authenticated and request.user.is_admin)
+            request.user.is_authenticated and request.user.is_admin)
 
 
 class IsAuthorOrModeratorOrReadOnly(BasePermission):
